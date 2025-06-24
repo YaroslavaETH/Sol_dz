0d9a9646e65e75d73d590b1ef628750a7e742c06
Yaroslava Yadrova <yadrovays@bcs.ru>
2025-06-24 21:51:18 +0700

    готовые тесты, комментарии, до оптимизации

--- src/SellNicknames.sol
+++ src/SellNicknames.sol
@@ -2,36 +2,55 @@
 pragma solidity ^0.8.29;
 
 contract SellNicknames {
-    string public nicknameSpace;
-    address public owner;
-    uint256 public price;
-    mapping (string nickname => address ownerNickname) public ownerOf;
+    string public nicknameSpace; // название пространства никнеймов
+    address public owner; // владелец пространства
+    uint256 public price; // текущая цена покупки ника
+    mapping (string nickname => address ownerNickname) public ownerOf; // владельцы ников
 
-    error NicknameIsBusy(address _ownerNickname);
-    error AnotherPrice(uint256 _currentPrice, uint256 _tryPrice);
-    error NotTheOwner();
-    error RandomCall();
+    
+    error NicknameIsBusy(address _ownerNickname); // ошибка при невозможности купить ник, т.к. он уже занят
+    error AnotherPrice(uint256 _currentPrice, uint256 _tryPrice); // ошибка при невозможности купить ник, т.к. не соотв. текущей цене
+    error NotTheOwner(); // ошибка при попытке выполнить операцию доступную только владельцу кем-либо еще
+    error RandomCall(); // ошибка обращения к контракту
 
-    event BuyNickname(address indexed _ownerNickname, string indexed _nickname);
-    event NewPrice(uint256 indexed _price);
-
+    event BuyNickname(address indexed _ownerNickname, string indexed _nickname); // событие покупки ника
+    event NewPrice(uint256 indexed _price); // событие установки новой цены
+    
+    /**
+     * @notice Конструктор, инициализирующий пространство никнеймов.
+     * @param _nicknameSpace Название пространства никнеймов.
+     */
     constructor(string memory _nicknameSpace) {
         nicknameSpace = _nicknameSpace;
         owner = msg.sender;
     }
-
+    
+    /**
+     * @notice Модификатор, который ограничивает выполнение функции только владельцем контракта.
+     * @dev Если вызывающий не является владельцем, транзакция будет отменена с ошибкой NotTheOwner.
+     */
     modifier onlyOwner {
         if(msg.sender != owner) revert NotTheOwner();
         _;
     }
 
+    /**
+     * @notice Устанавливает новую цену для покупки никнейма.
+     * @dev Может быть вызвана только владельцем контракта. Генерирует событие NewPrice.
+     * @param _price Новая цена для никнейма.
+     */
     function setPrice(uint256 _price) public onlyOwner{
         price = _price;
         emit NewPrice(_price);
     }
 
+    /**
+     * @notice Позволяет пользователю купить никнейм.
+     * @dev Пользователь должен отправить точное количество эфира, равное текущей цене.
+     * @dev Отменяется, если никнейм уже занят (NicknameIsBusy) или если отправленная сумма неверна (AnotherPrice).
+     * @param _nickname Желаемый никнейм для покупки.
+     */
     function buyNickname(string calldata _nickname) external payable {
-        //address _currentOwner = checkNickname(_nickname);
         address _currentOwner = ownerOf[_nickname];
         if(_currentOwner != address(0)) revert NicknameIsBusy(_currentOwner);
         uint256 _price = price;
@@ -42,9 +61,13 @@ contract SellNicknames {
         emit BuyNickname(msg.sender, _nickname);
     }
 
+    /**
+     * @notice Обрабатывает прямые переводы эфира на контракт.
+     * @dev Отменяет любые прямые переводы эфира с ошибкой RandomCall,
+     * так как покупка должна происходить через функцию buyNickname.
+     */
     receive() external payable {
-        //revert RandomCall();
-        revert NotTheOwner();
+        revert RandomCall();
     }
 
     // функции вывода денег с контракта не написана для уменьшения объема тестирования
--- test/SellNicknames.t.sol
+++ test/SellNicknames.t.sol
@@ -13,29 +13,53 @@ contract SellNicknamesTest is Test {
 
     address _otherAddress = makeAddr("_otherAddress");
 
+    /**
+     * @notice Настраивает тестовое окружение перед каждым тестом.
+     * @dev Развертывает новый экземпляр контракта SellNicknames с пространством имен "TestSpace".
+     */
     function setUp() public {
         sellNicknames = new SellNicknames("TestSpace");
     }
 
+    /**
+     * @notice Проверяет, что владелец контракта установлен правильно.
+     * @dev Убеждается, что `owner()` возвращает адрес тестового контракта.
+     */
     function test_owner() public {
         assertEq(sellNicknames.owner(), address(this));
     }
 
+    /**
+     * @notice Проверяет, что пространство никнеймов установлено правильно.
+     * @dev Убеждается, что `nicknameSpace()` возвращает "TestSpace", заданное в конструкторе.
+     */
     function test_nicknameSpace() public {
         assertEq(sellNicknames.nicknameSpace(), "TestSpace");
     }
 
+    /**
+     * @notice Фаззинг-тест для функции `setPrice`.
+     * @dev Проверяет, что функция `setPrice` корректно устанавливает цену для различных значений.
+     */
     function testFuzz_setPrice(uint256 x) public {
         sellNicknames.setPrice(x);
         assertEq(sellNicknames.price(), x);
     }
 
+    /**
+     * @notice Проверяет, что `setPrice` не может быть вызвана не владельцем.
+     * @dev Ожидает реверт с ошибкой `NotTheOwner`, если `setPrice` вызывается с другого адреса.
+     */
     function test_setPriceNotOwner() public {
         vm.expectRevert(SellNicknames.NotTheOwner.selector);
         vm.prank(address(0));
         sellNicknames.setPrice(10);
     }
 
+    /**
+     * @notice Проверяет, что при установке новой цены генерируется событие `NewPrice`.
+     * @dev Убеждается, что генерируется событие `NewPrice` при установке новой цены.
+     */
     function test_setPriceEventNewPrice() public {
         vm.expectEmit(true, true, true, false);
         uint256 _newPrice = 14;
@@ -43,6 +67,10 @@ contract SellNicknamesTest is Test {
         sellNicknames.setPrice(_newPrice);
     }
 
+    /**
+     * @notice Проверяет успешную покупку никнейма.
+     * @dev Убеждается, что генерируется событие `BuyNickname` и что владелец никнейма правильно установлен.
+     */
     function test_buyNickname() public {
         string memory _nickname = "Zevs";
         vm.expectEmit(true, true, true, false);
@@ -53,6 +81,10 @@ contract SellNicknamesTest is Test {
         vm.assertEq(sellNicknames.ownerOf(_nickname), _otherAddress);
     }
 
+    /**
+     * @notice Проверяет сценарий, когда никнейм уже занят.
+     * @dev Сначала покупает ник, затем пытается купить его снова и ожидает реверт с ошибкой `NicknameIsBusy`.
+     */
     function test_buyNicknameNicknameIsBusy() public {
         string memory _nickname = "Zevs";
         uint256 _price = sellNicknames.price();
@@ -67,6 +99,10 @@ contract SellNicknamesTest is Test {
         sellNicknames.buyNickname{value: _price}(_nickname);
     }
 
+    /**
+     * @notice Проверяет сценарий, когда для покупки отправлена неверная сумма.
+     * @dev Пытается купить ник, отправив больше эфира, чем текущая цена, и ожидает реверт с ошибкой `AnotherPrice`.
+     */
     function test_buyNicknameAnotherPrice() public {
         string memory _nickname = "Zevs";
         uint256 _currentprice = sellNicknames.price();
@@ -81,11 +117,16 @@ contract SellNicknamesTest is Test {
         sellNicknames.buyNickname{value: _price}(_nickname);
     }
 
+    /**
+     * @notice Проверяет, что прямой перевод эфира на контракт отклоняется.
+     * @dev Отправляет прямой перевод эфира на адрес контракта и проверяет, что транзакция отменяется с ошибкой `RandomCall`.
+     */
     function test_receive() public {
-        hoax(_otherAddress);
-        vm.expectRevert(SellNicknames.RandomCall.selector);        
-        (bool success, ) = address(sellNicknames).call{value: 1}("");
-        //vm.assertFalse(success);
-        //vm.assertEq(address(sellNicknames).balance, 0);
+        hoax(_otherAddress);      
+        (bool success, bytes memory result) = address(sellNicknames).call{value: 1}("");
+        vm.assertFalse(success);
+        bytes4 expectedSelector = bytes4(keccak256("RandomCall()"));
+        bytes4 receivedSelector = bytes4(result);        
+        vm.assertEq(receivedSelector, expectedSelector, "Should revert with RandomCall error");
     }
 }
